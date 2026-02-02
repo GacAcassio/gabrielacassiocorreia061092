645b6ee960a53f8455cab90086013eb7c390f5cd
Gabriel Acassio <acassiocorreia03@gmail.com>
2026-02-01 21:08:22 -0300

    Add Notification system NotificationContainer NotificationToast components

--- frontend/src/components/index.ts
+++ frontend/src/components/index.ts
@@ -1,3 +1,5 @@
 export { default as Loading } from './Loading';
 export { default as ErrorMessage } from './ErrorMessage';
 export { default as ArtistCard } from './ArtistCard';
+export { default as NotificationToast } from './NotificationToast';
+export { default as NotificationContainer } from './NotificationContainer';
--- frontend/src/models/index.ts
+++ frontend/src/models/index.ts
@@ -1,3 +1,5 @@
 export * from './Artist';
 export * from './Album';
 export * from './Auth';
+export * from './Page';
+export * from './Notification';
